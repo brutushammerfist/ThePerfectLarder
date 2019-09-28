2b50bd53c11edcdff7c1f7193ff7fecdd99dbc25
dtiller <dtill009@odu.edu>
2019-09-28 15:51:36 -0400

    Updated Recipes py kv test

--- application/src/recipes.py
+++ application/src/recipes.py
@@ -11,21 +11,26 @@
 import kivy
 kivy.require('1.11.1')
 from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
 
 class Recipes(Screen):
 	pass
 
+	#recipeName = ObjectProperty(None)
+	#ingredients = ObjectProperty(None)
+	#cookDirections = ObjectProperty(None)
+
 	# Will likely be done on the server side
-	# def recommendRecipes(usersInventory)
+	#def recommendRecipes(self):
 		# query the recipe database for recipes that match the users inventory
 		# display to users
 		
 		
-	# def userRecipe(user recipe)
-		# add recipe to the database
+	#def userRecipe(self):
+		# send (self.recipeName.text, self.ingredients.text, self.cookDirections.text) to database
 		
 		
-	#def verifyRecipeWasUsed(recipe, items, verification, usersInventory)
+	#def verifyRecipeWasUsed(self):
 		# remove the items that were used in the recipe from the inventory
 		
 class AddRecipe(Screen):         #part of recipes
--- application/testing/test_recipes.py
+++ application/testing/test_recipes.py
@@ -12,16 +12,16 @@ import Recipes
 class TestRecipes(unittest.TestCase):
 
 	# Will likely be done on the server side
-	# def test_recommendRecipes(usersInventory)
+	# def test_recommendRecipes(self):
 		# query the recipe database for recipes that match the users inventory
 		# display to users
 		
 		
-	# def test_userRecipe(user recipe)
+	# def test_userRecipe(self):
 		# add recipe to the database
 		
 		
-	#def test_verifyRecipeWasUsed(recipe, items, verification, usersInventory)
+	#def test_verifyRecipeWasUsed(self):
 		# remove the items that were used in the recipe from the inventory
 		
 #need below to run tests
