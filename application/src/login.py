5b9241a9286917a832ca4bdf1992550a79120535
mperry <mperry@411orangef19-mgmt.cs.odu.edu>
2019-09-30 16:28:07 +0000

    give url for request a connection adapter

--- application/src/login.py
+++ application/src/login.py
@@ -49,7 +49,7 @@ class Login(Screen):
             'password' : hash(self.ids.userPassword.text)
         }
         
-        response = requests.post('411orangef19-mgmt.cs.odu.edu:8000/login', headers=headers, data=json.dumps(payload)).json()
+        response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/login', headers=headers, data=json.dumps(payload)).json()
         
         #response = {'data' : 'Incorrect password.'}
         
