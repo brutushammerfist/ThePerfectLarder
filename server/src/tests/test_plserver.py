d4ad2f4d9edade8411753128bad649a1884616db
matthew perry <mperry@cs.odu.edu>
2019-10-02 13:14:52 -0400

    changing tests in test_plserver.py

--- server/src/tests/test_plserver.py
+++ server/src/tests/test_plserver.py
@@ -13,22 +13,22 @@ class test_plserver(TestCase):
     def test_login_logout(self):
         response = self.client.post("/login", data=dict(username='username', password=hash('password')))
         
-        self.assertEqual(response.code, 200)
+        self.assert200(response, 'Successful login failed.')
         self.assertEqual("Successful login.", response.data)
         
         response = self.client.post("/logout", data=dict(username='username'))
         
-        self.assertEqual(response.code, 200)
+        self.assert200(response, 'Succesful logout failed.')
         self.assertEqual("Successful logout.", response.data)
         
         response = self.client.post("/login", data=dict(username='abc', password='password'))
         
-        self.assertEqual(response.code, 401)
+        self.assert401(response, 'Invalid username failed.')
         self.assertEqual("Invalid username.", response.data)
         
         response = self.client.post("/login", data=dict(username='username', password='abc'))
         
-        self.assertEqual(response.code, 401)
+        self.assert401(response, 'Incorrect password failed.')
         self.assertEqual("Incorrect password.", response.data)
         
     def test_addItem(self):
