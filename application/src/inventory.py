8beb865d7ef7e11b3cffc3f8ebdfaa5d40acaaa3
dtiller <dtill009@odu.edu>
2019-10-02 16:04:50 -0400

    added popups and fucntionality to deleteitems and additems

--- application/src/inventory.py
+++ application/src/inventory.py
@@ -11,6 +11,9 @@ import kivy
 kivy.require('1.11.1')
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class Inventory(Screen):
 	pass
@@ -22,6 +25,22 @@ class Inventory(Screen):
 	
 	def addItems (self):
 	
+		#popup to confirm item was added
+		nameContent = GridLayout(cols=1)
+		nameContent.add_widget(Label(text= self.itemName.text + ' added to your inventory'))
+		nameButton = Button(text='OK')
+		nameContent.add_widget(nameButton)
+		addItemPopup = Popup(title='Added Item', content=nameContent, auto_dismiss=False)
+		nameButton.bind(on_press=addItemPopup.dismiss)
+		
+		#popup to let the user know the item was not added 
+		nameContent = GridLayout(cols=1)
+		nameContent.add_widget(Label(text= self.itemName.text + ' not added to your inventory'))
+		nameButton = Button(text='OK')
+		nameContent.add_widget(nameButton)
+		itemNotaddedPopup = Popup(title='Item Not Added', content=nameContent, auto_dismiss=False)
+		nameButton.bind(on_press=itemNotaddedPopup.dismiss)
+		
 		headers = {'Content-Type' : 'application/json'}
         
 		payload = {
@@ -33,14 +52,33 @@ class Inventory(Screen):
         
 		response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/addItem', headers=headers, data=json.dumps(payload)).json()
         
-		#clearing input text feild
-		self.itemName.text = ""
-		self.quantity.text = ""
-		self.expirationDate.text = ""
-		self.storageLocation.text = ""
+		if response['data'] == 'Item added.':
+			self.addItemPopup.open()
+			self.itemName.text = ""
+			self.quantity.text = ""
+			self.expirationDate.text = ""
+			self.storageLocation.text = ""
+		else:
+			self.itemNotaddedPopup.open()
 	
 	
 	def deleteItems(self):
+	
+		#popup to confirm item was deleted
+		nameContent = GridLayout(cols=1)
+		nameContent.add_widget(Label(text= self.itemName.text + ' was deleted from your inventory'))
+		nameButton = Button(text='OK')
+		nameContent.add_widget(nameButton)
+		delItemPopup = Popup(title='Deleted Item', content=nameContent, auto_dismiss=False)
+		nameButton.bind(on_press=delItemPopup.dismiss)
+		
+		#popup to let the user know the item was not deleted 
+		nameContent = GridLayout(cols=1)
+		nameContent.add_widget(Label(text= self.itemName.text + ' was not deleted from your inventory'))
+		nameButton = Button(text='OK')
+		nameContent.add_widget(nameButton)
+		itemNotdelPopup = Popup(title='Item Not Deleted', content=nameContent, auto_dismiss=False)
+		nameButton.bind(on_press=itemNotdelPopup.dismiss)
 		
 		headers = {'Content-Type' : 'application/json'}
         
@@ -52,11 +90,13 @@ class Inventory(Screen):
 		
 		response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/delItem', headers=headers, data=json.dumps(payload)).json()
 		
-		#clearing input text feild
-		self.itemName.text = ""
-		self.quantity.text = ""
-		self.storageLocation.text = ""
-	
+		if response['data'] == 'Item Deleted.':
+			self.delItemPopup.open()
+			self.itemName.text = ""
+			self.quantity.text = ""
+			self.storageLocation.text = ""
+		else:
+			self.itemNotdelPopup.open()
 	
 	#def viewInventory(self):
 		#display the inventory to the userInventory
