28006a7600894b98da63b03d3b0e67d4b229e864
matthew perry <mperry@cs.odu.edu>
2019-10-02 13:27:48 -0400

    setting flask app to actually return status codes

--- server/src/plserver/database.py
+++ server/src/plserver/database.py
@@ -22,18 +22,19 @@ class Database():
             payload = {
                 'data' : 'Invalid username.'
             }
+	    return json.dumps(payload), 401
         else:
             if password == result['password']:
                 payload = {
                     'data' : 'Successful login.'
                 }
+		return json.dumps(payload), 200
             else:
                 payload = {
                     'data' : 'Incorrect password.'
                 }
-            
-        return json.dumps(payload)
-    
+		return json.dumps(payload), 401            
+   
     def addItem(self):
         pass
     
