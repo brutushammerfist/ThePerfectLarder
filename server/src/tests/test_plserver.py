69d11618336895c8b88f7ad4b646d7454516f31c
mperry <mperr006@odu.edu>
2019-10-02 23:58:24 -0400

    created more tests for the server, adding/deleting/getting items

--- server/src/tests/test_plserver.py
+++ server/src/tests/test_plserver.py
@@ -35,14 +35,15 @@ class test_plserver(TestCase):
         self.assert401(response, 'Incorrect password failed.')
         self.assertEqual("Incorrect password.", data['data'])
         
-    def test_addItem_getItem(self):
-        response = self.client.post('/addItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(name='Strawberry', quantity='1', measurement='lbs', location='fridge')))
+    def test_addItem_getItem_delItem(self):
+        response = self.client.post('/addItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name='Strawberry', quantity='1', measurement='lbs', location='fridge', expDate='2020-01-01')))
         data = json.loads(response.data)
         
         self.assert200(response, 'Adding item failed.')
         self.assertEqual("Successfully added item to inventory.", data['data'])
         
-        response = self.client.post('/getItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(name='Strawberry')))
+        response = self.client.post('/getItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name='Strawberry')))
+        data = json.loads(response.data)
         
         self.assert200(response, 'Getting item failed.')
         self.assertEqual("Successfully pulled item from inventory.", data['data'])
@@ -50,11 +51,52 @@ class test_plserver(TestCase):
         self.assertEqual("1", data['item']['quantity'])
         self.assertEqual("lbs", data['item']['measurement'])
         self.assertEqual("fridge", data['item']['location'])
+        self.assertEqual("2020-01-01", data['item']['expDate'])
         
-    def test_delItem(self):
-        pass
+        response = self.client.post('/delItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name="Strawberry")))
+        data = json.loads(response.data)
+        
+        self.assert200(response, "Deleting item failed.")
+        self.assertEqual("Successfully deleted item from inventory.", data['data'])
+        
+        response = self.client.post('/delItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name="Stewberry")))
+        data = json.loads(response.data)
+        
+        self.assert401(response, "Found item that doesn't exist.")
+        self.assertEqual("Found item that doesn't exist", data['data'])
+        
+        response = self.client.post('/getItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name="Strawberry")))
+        data = json.loads(response.data)
+        
+        self.assert401(response, "Item does not exist.")
+        self.assertEqual("Item does not exist.", data['data'])
     
     def test_getInventory(self):
+        response = self.client.post('/addItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name='Strawberry', quantity='1', measurement='lbs', location='fridge', expDate='2020-01-01')))
+        response = self.client.post('/addItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name='Ground Beef', quantity='3', measurement='lbs', location='fridge', expDate='2020-02-02')))
+        response = self.client.post('/addItem', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1, name='Milk', quantity='1', measurement='gallon', location='fridge', expDate='2020-03-03')))
+        
+        response = self.client.post('/getInventory', headers={'Content-Type':'application/json'}, data=json.dumps(dict(userID=1)))
+        data = json.loads(response.data)
+        
+        self.assertEqual(3, len(data['data']))
+        self.assertEqual('Ground Beef', data['data'][0]['name'])
+        self.assertEqual('3', data['data'][0]['quantity'])
+        self.assertEqual('lbs', data['data'][0]['measurement'])
+        self.assertEqual('fridge', data['data'][0]['location'])
+        self.assertEqual('2020-01-01', data['data'][0]['expDate'])
+        self.assertEqual('Milk', data['data'][1]['name'])
+        self.assertEqual('1', data['data'][1]['quantity'])
+        self.assertEqual('gallon', data['data'][1]['measurement'])
+        self.assertEqual('fridge', data['data'][1]['location'])
+        self.assertEqual('2020-02-02', data['data'][1]['expDate'])
+        self.assertEqual('Strawberry', data['data'][2]['name'])
+        self.assertEqual('1', data['data'][2]['quantity'])
+        self.assertEqual('lbs', data['data'][2]['measurement'])
+        self.assertEqual('fridge', data['data'][2]['location'])
+        self.assertEqual('2020-03-03', data['data'][2]['expDate'])
+        
+    def test_perfectLarder(self):
         pass
 
 if __name__ == '__main__':
