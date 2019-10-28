50ec6a78db5f9f7c2734aa73032b4ef622cf5b54
aadeniran <aadeniran@cs.odu.edu>
2019-10-28 14:12:23 -0400

    checking if email or username already exist

--- server/src/plserver/database.py
+++ server/src/plserver/database.py
@@ -56,37 +56,35 @@ class Database():
         usrn = (str(content['username']),)
         self.cursor.execute(sql,usrn)
         result1 = self.cursor.fetchall()
-
-
+        
+        
+            
         sqlTwo = "SELECT id FROM `Users` WHERE `Users`.`email` = %s"
         usre = (str(content['useremail']),)
         self.cursor.execute(sqlTwo,usre)
         result2 = self.cursor.fetchall()
-
+        
+        if(len(result1) != 0 and result2 != 0):
+            return (json.dumps(dict(data='3')), 401)
+        if(len(result1) != 0 ):
+            return (json.dumps(dict(data='1')), 401)
+        if(len(result2) != 0):
+            return (json.dumps(dict(data='2')), 401)
+        
         usrp = (str(content['password']),)
-
+        
         if len(result1) == 0 and len(result2) == 0:
             sql = "SELECT inventoryID FROM Users ORDER BY inventoryID DESC"
             self.cursor.execute(sql)
             result = self.cursor.fetchall()
 
             inventoryID = result[0][0] + 1
-            
-            storageLocations = {
-                'locations' : ['Fridge', 'Freezer', 'Dry']
-            }
 
-            sqlInsert = "INSERT INTO Users (name, email, phone, username, password, inventoryID, storageLocations) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-            val = (content['name'], content['useremail'], content['phone'], content['username'], content['password'], inventoryID, storageLocations, )
+            sqlInsert = "INSERT INTO Users (name, email, phone, username, password, inventoryID) VALUES (%s, %s, %s, %s, %s, %s)"
+            val = (content['name'], content['useremail'], content['phone'], content['username'], content['password'], inventoryID, )
             self.cursor.execute(sqlInsert, val)
             result = self.connector.commit()
             return (json.dumps(dict(data='0')), 200)
-        elif(len(result1) > 0):
-            return (json.dumps(dict(data='1')), 401)
-        elif(len(result2) > 0):
-            return (json.dumps(dict(data='2')), 401)
-        else:
-            return (json.dumps(dict(data='3')), 401)
 
     def addItem(self, content):
         sql = "SELECT (inventoryID) FROM Users WHERE id = %s"
