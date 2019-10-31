d72f9658411c9bdd1450983c7d07749c322ce9f4
aadeniran <aaden001@odu.edu>
2019-10-31 15:17:09 -0400

    Made the login screen displays pop up on empty inputs

--- application/src/login.py
+++ application/src/login.py
@@ -37,7 +37,7 @@ class Login(Screen):
     nameContent.add_widget(nameButton)
     userNamePopup = Popup(title='Invalid Username', content=nameContent, auto_dismiss=False, size_hint=(.8, .2))
     nameButton.bind(on_press=userNamePopup.dismiss)
-
+    
     passContent = GridLayout(cols=1)
     passContent.add_widget(Label(text='The password you entered was incorrect.'))
     passButton = Button(text='OK')
@@ -51,37 +51,62 @@ class Login(Screen):
     svrContent.add_widget(svrButton)
     svrPopup = Popup(title='Cannot Connect', content=svrContent, auto_dismiss=False, size_hint=(.85, .2))
     svrButton.bind(on_press=svrPopup.dismiss)
+    
+    nameEmptyContent = GridLayout(cols=1)
+    nameEmptyContent.add_widget(Label(text='The username field  cannot be empty.'))
+    nameEmptyButton = Button(text='OK')
+    nameEmptyContent.add_widget(nameEmptyButton)
+    nameEmptyPopup = Popup(title='Empty input in Username', content=nameEmptyContent, auto_dismiss=False, size_hint=(.8, .2))
+    nameEmptyButton.bind(on_press=nameEmptyPopup.dismiss)
+    
+    passWordEmptyContent = GridLayout(cols=1)
+    passWordEmptyContent.add_widget(Label(text='The password field cannot be empty.'))
+    passWordEmptyButton = Button(text='OK')
+    passWordEmptyContent.add_widget(passWordEmptyButton)
+    passWordEmptyPopup = Popup(title='Empty input in Password', content=passWordEmptyContent, auto_dismiss=False, size_hint=(.8, .2))
+    passWordEmptyButton.bind(on_press=passWordEmptyPopup.dismiss)
 
     def userLogin(self):
-        # Will most likely not change for most POST requests
-        headers = {'Content-Type': 'application/json'}
-
-        payload = {
-            'username': self.ids.userName.text,
-            'password': self.ids.userPassword.text
-        }
-        try:
-            response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/login', headers=headers,
-                                     data=json.dumps(payload)).json()
-        except:
-            response = {'data': 'Unable to Connect.'}
-
-        # response = {'data' : 'Incorrect password.'}
-
-        # if incorrect, clear password and have them try again
-        if response['data'] == 'Successful login.':
-            App.get_running_app().userID = response['userID']
-            App.get_running_app().userMeasurement = response['measureType']
-            App.get_running_app().userNotif = response['notifPref']
-            App.get_running_app().storageLocations = json.loads(response['locations'])['locations']
-            self.ids.userName.text = ""
-            self.ids.userPassword.text = ""
-            self.manager.current = 'homescreen'
-        elif response['data'] == 'Invalid username.':
-            self.ids.userPassword.text = ""
-            self.userNamePopup.open()
-        elif response['data'] == 'Incorrect password.':
-            self.ids.userPassword.text = ""
-            self.userPassPopup.open()
-        elif response['data'] == 'Unable to Connect.':
-            self.svrPopup.open()
+        usersName = self.ids.userName.text
+        passWord = self.ids.userPassword.text
+        
+        if(usersName != ""):
+            
+            if(passWord != ""):
+                # Will most likely not change for most POST requests
+                headers = {'Content-Type': 'application/json'}
+        
+                payload = {
+                    'username': usersName,
+                    'password': passWord
+                }
+                try:
+                    response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/login', headers=headers,
+                                             data=json.dumps(payload)).json()
+                except:
+                    response = {'data': 'Unable to Connect.'}
+        
+                # response = {'data' : 'Incorrect password.'}
+        
+                # if incorrect, clear password and have them try again
+                if response['data'] == 'Successful login.':
+                    App.get_running_app().userID = response['userID']
+                    App.get_running_app().userMeasurement = response['measureType']
+                    App.get_running_app().userNotif = response['notifPref']
+                    App.get_running_app().storageLocations = json.loads(response['locations'])['locations']
+                    self.ids.userName.text = ""
+                    self.ids.userPassword.text = ""
+                    self.manager.current = 'homescreen'
+                elif response['data'] == 'Invalid username.':
+                    self.ids.userPassword.text = ""
+                    self.userNamePopup.open()
+                elif response['data'] == 'Incorrect password.':
+                    self.ids.userPassword.text = ""
+                    self.userPassPopup.open()
+                elif response['data'] == 'Unable to Connect.':
+                    self.svrPopup.open()
+            else:
+                self.passWordEmptyPopup.open()
+        else:
+            self.nameEmptyPopup.open()
+            
