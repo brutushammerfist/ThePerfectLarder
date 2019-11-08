446ab724fa818157ccf8d448924e64ce8a16c21d
aadeniran <aaden001@odu.edu>
2019-11-08 14:52:18 -0500

    debugging error format itemshare

--- application/src/itemshare.py
+++ application/src/itemshare.py
@@ -44,14 +44,14 @@ class ItemShare(Screen):
 		string_weekAhead =  week_ahead.strftime("%Y") + "-" + week_ahead.strftime("%m") + "-" + week_ahead.strftime("%d")
 
 		headers = {'Content-Type' : 'application/json'}
-		API_ENDPOINT = 'http://411orangef19-mgmt.cs.odu.edu:8000/getItemsAboutToExpire'
+		#API_ENDPOINT = 'http://411orangef19-mgmt.cs.odu.edu:8000/getItemsAboutToExpire'
 		payload = {
 			'userID' : App.get_running_app().userID,
 			'currentDate' : stringCurrent,
 			'currentWeekAhead' : string_weekAhead
 		}
 		print(App.get_running_app().userID)
-		response = requests.post(url = API_ENDPOINT, headers=headers, data=json.dumps(payload)).json()
+		response = requests.post('http://411orangef19-mgmt.cs.odu.edu:8000/getItemsAboutToExpire', headers=headers, data=json.dumps(payload)).json()
 
 		resEmptyCheck = not response
 		print(resEmptyCheck)
