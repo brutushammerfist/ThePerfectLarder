97262ccc02ba8ee7d8ad1e19780a424c0f60f3d6
jschneider <jschneider@egipan>
2019-11-15 13:57:17 -0500

    Resolved GUI issue with feedback window staying open in the background

--- application/src/feedback.py
+++ application/src/feedback.py
@@ -4,15 +4,12 @@
 # By: Adeniyi Adeniran, Chris Whitney, Collin DeWaters, Derek Tiller, Jonathan Schneider
 #     Matthew Perry, Melanie Devoe, and Zachery Miller 
 
-# setup GUI(kivy)
-
 import kivy, smtplib, ssl, email
 from _ast import Or
-# This line does not work with SDK
-#from pip._vendor.distlib.util import OR
 kivy.require('1.11.1')
 from kivy.uix.screenmanager import Screen
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
@@ -28,13 +25,42 @@ class Feedback(Screen):
 	errorMsg = Label()
 	
 	@staticmethod
-	def widgetWithMessage(message):
+	def errorWidget(message):
 		widget = Label(text=message)
-		#widget.font_size = 45
+		widget.font_size = 45
 		widget.bold = True
 		widget.color = (1, 0, 0,0.75)
 		widget.pos_hint = {"top":.66}
 		return widget
+	
+	@staticmethod
+	def successWidget(message):
+		widget = Label(text=message)
+		widget.font_size = 45
+		widget.bold = True
+		widget.color = (0, 1, 0,0.75)
+		widget.pos_hint = {"top":.66}
+		return widget
+	
+	@staticmethod
+	def popUpMessage(header, message):
+			content = GridLayout()
+			content.cols = 1
+			content.add_widget(
+			    Label(text=message, halign='center', valign='top', font_size=60)
+			)
+			ok_button = Button(text='Ok', font_size=60)
+			content.add_widget(ok_button)
+			
+			popup = Popup(
+			    title=header,
+			    content=content,
+			    size_hint=(None, None),
+			    size=(Window.width / 3, Window.height / 3),
+			    auto_dismiss=True,
+			)
+			ok_button.bind(on_press=popup.dismiss)
+			popup.open()
 	
 	def validateFeedback(self, message, rating):
 		messageValid = 0
@@ -60,17 +86,24 @@ class Feedback(Screen):
 		
 		if ( not messageValid and not ratingValid ):
 			print ('invalid message and rating')
-			self.errorMsg = self.widgetWithMessage( 'Please leave a rating and a comment :-)' )
-			self.add_widget( self.errorMsg )	
+			self.errorMsg = self.errorWidget( 'Please leave a rating and a comment :-)' )
+			self.add_widget( self.errorMsg )
 		elif ( not messageValid ):
 			print ('invalid message')
-			self.errorMsg = self.widgetWithMessage( 'Please leave a comment :-)' )
+			self.errorMsg = self.errorWidget( 'Please leave a comment :-)' )
 			self.add_widget( self.errorMsg )
 		elif ( not ratingValid ):
 			print ('invalid rating')
-			self.errorMsg = self.widgetWithMessage( 'Please leave a rating :-)' )
+			self.errorMsg = self.errorWidget( 'Please leave a rating :-)' )
 			self.add_widget( self.errorMsg )
+			#self.popUpMessage("Error", "Please leave a rating :-)")
 
+
+		else:
+			print ('feedback valid')
+			self.successMsg = self.successWidget( 'Thank you for submitting feedback :-)' )
+			self.add_widget( self.successMsg )
+		
 		return ( messageValid and ratingValid )
 
 	def submitFeedback(self):
@@ -78,7 +111,6 @@ class Feedback(Screen):
 		rating = int(self.ids.toggleValue.text)
 		
 		if ( self.validateFeedback(message, rating) ):
-			print('feedback valid')
 			password = 'plfeedback'
 			sender_email = 'perfect.larder.feedback@gmail.com'
 			receiver_email = "perfect.larder@gmail.com"
@@ -96,7 +128,7 @@ class Feedback(Screen):
 			with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
 				server.login(sender_email, password)
 				server.sendmail(sender_email, receiver_email, text)			
-			
+
 		else:
 			print('feedback invalid')
 			
