45d9497f20634e5cb5dac0bf5330c324d5dad631
aadeniran <aaden001@odu.edu>
2019-11-29 19:46:43 -0500

    adjusting reject functionality

--- server/src/plserver/database.py
+++ server/src/plserver/database.py
@@ -984,7 +984,7 @@ class Database():
         sharedItemId = content['itemId']
         response = "no"
         
-        sql = "UPDATE SharedItem response = %s WHERE shareditemId = %s AND userId = %s"
+        sql = "UPDATE SharedItem SET response = %s WHERE shareditemId = %s AND userId = %s"
         val = (response,sharedItemId,userId, )
         self.cursor.execute(sql, val)
         result = self.connector.commit()
